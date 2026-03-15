6038d39ca898a946b268af52f02325521e2e8f4c
itzSumma <itzSumma11@gmail.com>
2026-03-15 19:39:10 +0600

    impliment toggle messege on follow/unfollow/reset buttons

--- Follow-Button-App/src/App.jsx
+++ Follow-Button-App/src/App.jsx
@@ -4,34 +4,47 @@ import "./App.css";
 function App() {
   const [likes, setLikes] = useState(0);
   const [follow, setFollow] = useState(false);
-  function handleClick (){
-  setFollow(true);
-  setLikes(likes+1);
-
+  const [message, setMessage] = useState(null);
+  function handleClick() {
+    setFollow(true);
+    setLikes(likes + 1);
+    setMessage("follow");
   }
 
-  function handleClick2 (){
-    if(likes >0 )
-    {
-        setFollow(false)
-       setLikes(likes-1)
+  function handleClick2() {
+    if (likes > 0) {
+      setFollow(false);
+      setLikes(likes - 1);
+      setMessage("unfollow");
     }
-        
   }
-  function handleClick3 (){
+  function handleClick3() {
     setFollow(false);
-    setLikes(0)
+    setLikes(0);
+    setMessage(null);
   }
 
   return (
     <div className="container">
       <h1>Follow Button App</h1>
       <div className="child-container">
-      <button className="button" onClick={handleClick}>❤️{follow ? "Following" : "Follow"}</button>
-      <button className="button" onClick={handleClick2}>Unfollow</button>
-      <button className="button" onClick={handleClick3}>Reset</button>
-      <p className="card">Total Likes:{likes}</p>
-      </div>
+        <button className="button" onClick={handleClick}>
+          ❤️{follow ? "Following" : "Follow"}
+        </button>
+        <button className="button" onClick={handleClick2}>
+          Unfollow
+        </button>
+        <button className="button" onClick={handleClick3}>
+          Reset
+        </button>
+        <p className="card">Total Likes:{likes}</p>
+
+     
+      <div id="message">
+        {message === "follow" && <p className="card">You have followed</p>}
+        {message === "unfollow" && <p className="card">You have unfollowed</p>}
+        </div>
+         </div>
     </div>
   );
 }
