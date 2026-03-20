777b9c19e8a55fce219e12f490dcea59b4af67cd
itzSumma <itzSumma11@gmail.com>
2026-03-20 22:19:48 +0600

    tailwind css installed successfully

---
+++ React-Analytics-Dashboard/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <h1 className="text-amber-200 text-4xl font-bold">
+      React + Tailwind is working 
+      <p className='text-3xl font-medium text-red-500 mt-5'>Use suggestions</p>
+    </h1>
+    </div>
+  )
+}
+
+export default App
