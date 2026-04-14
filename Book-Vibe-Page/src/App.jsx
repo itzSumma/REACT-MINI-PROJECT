d896990803a8b72185c5f2a8f94b1e28f1c9ce85
itzSumma <itzSumma11@gmail.com>
2026-04-14 10:09:02 +0600

    tailwind and daisy ui installed

--- Book-Vibe-Page/src/App.jsx
+++ Book-Vibe-Page/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
   return (
     <>
      <div>
+<h2 className='bg-blue-500 mt-10 container mx-auto'>Hello</h2>
+<br />
 
+<button className='btn btn-primary'>add</button>
      </div>
     </>
   )
